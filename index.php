544c089bac7392435fb7f0cd26233259129f2631
clicktic <sambalu03@gmail.com>
2013-09-17 14:01:46 -0400

    Add code in index.php and created CSSfile

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-Welcome to Project 1 for CSCI E-15 
+<!DOCTYPE html>
+<html>
+	<head>
+		<link href='http://fonts.googleapis.com/css?family=Graduate' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Rationale' rel='stylesheet' type='text/css'>
+		<style type="text/css">
+		@import url("style.css");
+		</style>
+		<title>Welcome to Project 1</title>
+	</head>
+	<body>
+		<div><h2>Suresh Balu</h2></div>
+		<div><h3>About me</h3></div>
+		<div class="box fade-in one"><p><h4>Hello, I am Suresh from New York. Recently, I graduated with my undergrad degree in information system management and found similar program here at Harvard for master degree. For past few years, I have taken the role of a management consultant to assist enterprises with their complex business problem and to design logical solutions. Beside academia and professional life, I enjoy reading, philosophy and anything to do with consciousness.</p>
+
+		<p>Throughout, my research I came across many posting referring the use of a Sublime text editor. Since I am a newbie in the domain of development, I choose to go with Sublime text editor and so far I like it. It is better than notepad, at least I think; you might be disagreed with me.</p><h4></div>
+	
+	</body>
+</html>
